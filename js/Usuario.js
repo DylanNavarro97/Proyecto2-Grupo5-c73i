5343c5dee20e6bababfb3fa20b220cbb195bcded
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2023-12-13 17:08:13 -0300

    agregando getters y setters a la clase Usuario

--- js/Usuario.js
+++ js/Usuario.js
@@ -6,4 +6,20 @@ export class Usuario {
         this.#email = email;
         this.#contraseña = contraseña;
     }
+
+    get getEmail (){
+        return this.#email
+    }
+
+    set setEmail (nuevoEmail){
+        this.#email = nuevoEmail
+    }
+
+    get getContraseña(){
+        return this.#contraseña
+    }
+
+    set setContraseña (nuevaContraseña){
+        this.#email = nuevaContraseña
+    }
 }
