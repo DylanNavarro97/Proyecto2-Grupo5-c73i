5b78492c50c7f6736ae2db9c1cb30f098fea157b
Elias Juarez <juarezelias83@gmail.com>
2023-12-16 16:33:57 -0300

    se modifico los estilos y estructura de las tarjetas de canciones, junto con el READ

--- js/indexReadCanciones.js
+++ js/indexReadCanciones.js
@@ -4,15 +4,21 @@ for (let cancion of listaCancion) {
   const crearCard = (cancion) => {
     const contenedorCanciones = document.querySelector("#contenedorCanciones");
     contenedorCanciones.innerHTML += `
-        <li class="list-unstyled w-25 cancion">
-            <div class="d-flex justify-content-end flex-column w-100 h-100 musicaEfecto">
-                <img src=${cancion.linkImg} alt="">
-                <button class="btn border-0 btnPlay"><i class="fa-solid fa-play"></i></button>
-                <button class="btn border-0 btnAgregar" onclick="cancionFavorita('${cancion.id}')"><i class="fa-solid fa-circle-plus"></i></button>
-                <h6 class="ms-1 textoCancion">${cancion.cancion}</h6>
-                <h6 class="ms-1 textoCancion">${cancion.banda}</h6>
-            </div>
-        </li>
+          <div class="cancion">
+              <div class="d-flex justify-content-end flex-column w-100 h-100 musicaEfecto">
+                  <img src=${cancion.linkImg} alt="imagen de canción">
+                  <div class="d-flex justify-content-between align-items-center">
+                      <div>
+                          <h6 class="ms-1 textoCancion">${cancion.cancion}</h6>
+                          <h6 class="ms-1 textoCancion">${cancion.banda}</h6>
+                      </div>
+                      <div class="d-flex flex-column">
+                          <button class="btn border-0 btnPlay p-1"><i class="bi bi-play-fill w-100 fs-2"></i></button>
+                          <button class="btn border-0 btnAgregar p-1 mx-lg-2" onclick="cancionFavorita('${cancion.id}')"><i class="bi bi-plus-circle-fill fs-5"></i></button>
+                      </div>
+                  </div>
+              </div>
+          </div>
         `;
   };
   crearCard(cancion);
